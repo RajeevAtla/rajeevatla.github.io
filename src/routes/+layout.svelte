b3dc8c1129101a21591b964be2c777c93cf613e1
Rajeev Atla <rajeev@rajeevatla.com>
2026-01-27 01:15:09 -0500

    change font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
 		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,600;6..96,700&family=IBM+Plex+Mono:wght@500;600;700&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:opsz,wght@6..96,600;6..96,700&family=IBM+Plex+Mono:wght@500;600;700&family=Poiret+One&display=swap"
 	/>
 </svelte:head>
 
